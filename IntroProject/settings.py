24d17e5f883e4cd4acdf04ef7503126a408644ef
Jaime Miranda <jaime.miranda1@upr.edu>
2021-02-07 15:57:11 -0400

    Initial Setup.

--- IntroProject/settings.py
+++ IntroProject/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # App
+    'UPR_Grader.apps.UprGraderConfig'
 ]
 
 MIDDLEWARE = [
@@ -76,8 +79,12 @@ WSGI_APPLICATION = 'IntroProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'UPR_Grader_DB',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
---
+++ UPR_Grader/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class UprGraderConfig(AppConfig):
+    name = 'UPR_Grader'
