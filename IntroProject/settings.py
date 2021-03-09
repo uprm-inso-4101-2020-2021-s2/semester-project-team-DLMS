c63451e57c9aa19373c7f75482ecb1593b055d8b
Joey R. Hernandez Perez <46766573+joeyhernandez1@users.noreply.github.com>
2021-03-09 13:46:25 -0400

    Modified Heroku configurations

--- IntroProject/settings.py
+++ IntroProject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -89,6 +90,7 @@ DATABASES = {
     }
 }
 
+DATABASES['default'] = dj_database_url.config()
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
