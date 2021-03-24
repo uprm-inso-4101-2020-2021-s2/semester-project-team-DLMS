1285bea0100c4cf778f4f53c14b1d4e021e8dced
Joey R. Hernandez Perez <46766573+joeyhernandez1@users.noreply.github.com>
2021-03-23 20:44:26 -0400

    Updated data models

--- UPR_Grader/models.py
+++ UPR_Grader/models.py
@@ -11,10 +11,36 @@ class Students(models.Model):
     student_gpa = models.CharField(max_length=4, default=0.00)
     student_major_gpa = models.CharField(max_length=4, default=0.00)
 
+class Courses(models.Model):
+    course_credits = models.IntegerField()
+    course_name = models.CharField(max_length=50)
+
+class Program(models.Model):
+    program_name = models.CharField(max_length=100)
+    program_course_total = models.IntegerField()
+    program_credits = models.IntegerField()
+    program_department = models.CharField(max_length=100)
+
+class StudentCourses(models.Model):
+    course_grade = models.CharField(max_length=1)
+    course_professor = models.CharField(max_length=50)
+    course_is_concentration = models.BooleanField()
+    course_time_start = models.TimeField()
+    course_time_end = models.TimeField()
+    course_section = models.CharField(max_length=10)
+    course_semester = models.IntegerField()
+    student = models.OneToOneField('Students', on_delete=models.CASCADE)
+    course = models.OneToOneField('Courses', on_delete=models.CASCADE)
+
+class Program_Courses(models.Model):
+    program = models.OneToOneField('Program', on_delete=models.CASCADE)
+    course = models.OneToOneField('Courses', on_delete=models.CASCADE)
+
 class Enrolled_Courses(models.Model):
     course_code = models.CharField(max_length=9)
     course_title = models.CharField(max_length=100)
     course_credits = models.CharField(max_length=2)
+    student = models.OneToOneField('Students', on_delete=models.CASCADE)
 
 
 
